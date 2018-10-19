25086af2e87b300cd441588b2e1e6b7b9cf031b7
Marco Baldanza <marco@X303>
2018-10-19 20:39:57 +0100

    a few bits

--- pedigree_demo/urls.py
+++ pedigree_demo/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from pedigree import views
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('pedigree/', include('pedigree.urls')),
     path('members/', include('members.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
